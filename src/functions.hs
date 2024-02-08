fb6b8d1c756b6d15dc6391ab20ba1ca668a641fb
Raunak Redkar <raunak.redkar@gmail.com>
2024-02-08 11:57:39 +0100

    starting on median length

--- src/functions.hs
+++ src/functions.hs
@@ -10,5 +10,21 @@ listReverse :: [Int] -> [Int]
 listReverse [] = []
 listReverse (a:b) = listReverse b ++ [a]
 
+
+sortLen :: [String] -> [String]
+sortLen x = sortBy cmp x where 
+	cmp a b | n < m = LT
+		| n > m = GT
+		| otherwise = LT
+	  where n = length a
+	        m = length b
+
+-- get middle function
+
+getMiddle::[String] -> Int
+getMiddle (x) = length (x !! 0)
+
+
 medianLength :: [String] -> Int
+medianLength x = getMiddle(sortLen(x))
 
